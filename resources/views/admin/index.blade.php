cfddfe0b7aabca410589929366fcf9908a07ac2d
= <ergeysaevbars@mail.ru>
2021-01-03 20:02:53 +0600

    вьюха в админке

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,5 +3,31 @@
 @section('content')
     <div class="container">
         <h4>{{ __('admin.welcome_to_admin_panel') }}</h4>
+        <div class="row">
+            <div class="col col-sm-6">
+                <div class="card">
+                    <div class="card-header">
+                        __
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col col-sm-6">
+                <div class="card">
+                    <div class="card-header">
+                        Featured
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
