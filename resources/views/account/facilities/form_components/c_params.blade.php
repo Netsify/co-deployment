0f8fbd5646514c29252990579800dcf998608e0f
= <ergeysaevbars@mail.ru>
2021-04-13 23:17:16 +0600

    Убрал закоментированные строки из шаблона

--- resources/views/account/facilities/form_components/c_params.blade.php
+++ resources/views/account/facilities/form_components/c_params.blade.php
@@ -10,9 +10,6 @@
                                 selected-value="{{ optional($facility->compatibilityParam($param->id))->value ?? $param->default_val }}"
                                 id="c_param[{{ $param->id }}]"
                 ></c-param-slider>
-                {{--<input type="range" id="c_param[{{ $param->id }}]" name="c_param[{{ $param->id }}]" class="form-range"
-                       min="{{ $param->min_val }}" max="{{ $param->max_val }}"
-                       value="{{ optional($facility->compatibilityParam($param->id))->value ?? $param->default_val }}" id="customRange2">--}}
             </div>
             <c-param-description :descriptions="descriptions" type-id="{{ $param->id }}"></c-param-description>
         </div>
