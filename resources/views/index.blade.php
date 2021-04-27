badb62b993a9ec8f41a142e5ffe9ff5d1ba94bf0
= <ergeysaevbars@mail.ru>
2021-04-27 20:10:15 +0600

    Центрировал ссылки на главной

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,10 +15,10 @@
             <div class="col col-sm-6">
                 <div class="card bg-dark text-white">
                     <img src="{{ $road_image }}" class="card-img" alt="..." style="opacity: 0.67; height: 390px;">
-                    <div class="card-img-overlay">
+                    <div class="card-img-overlay text-center">
                         <br><br><br><br><br><br><br>
                         <a  class="btn btn-outline-light" href="{{ route('login') }}">
-                            <h3 class="card-title text-center">{{ __('dictionary.infrastructure_owners') }}</h3>
+                            <h3 class="card-title">{{ __('dictionary.infrastructure_owners') }}</h3>
                         </a>
                     </div>
                 </div>
@@ -26,10 +26,10 @@
             <div class="col col-sm-6">
                 <div class="card bg-dark text-white">
                     <img src="{{ $ict_image }}" class="card-img" alt="..." style="opacity: 0.67; height: 390px;">
-                    <div class="card-img-overlay">
+                    <div class="card-img-overlay text-center">
                         <br><br><br><br><br><br><br>
                         <a  class="btn btn-outline-light" href="{{ route('login') }}">
-                            <h3 class="card-title text-center">{{ __('dictionary.ict_operators') }}</h3>
+                            <h3 class="card-title">{{ __('dictionary.ict_operators') }}</h3>
                         </a>
                     </div>
                 </div>
