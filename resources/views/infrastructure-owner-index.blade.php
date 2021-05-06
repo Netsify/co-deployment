8c1022209260cfae9ac611c78afc803ea174fa05
= <ergeysaevbars@mail.ru>
2021-05-06 22:08:23 +0600

    Правки во вьюхе

--- resources/views/infrastructure-owner-index.blade.php
+++ resources/views/infrastructure-owner-index.blade.php
@@ -6,13 +6,13 @@
             @foreach($toolbar_items as $item)
                 <div class="col col-sm-4 mb-5">
                     <a href="{{ route($item['link']) }}" class="btn btn-outline-info">
-                        <div class="card" style="width: 21rem">
+                        <div class="card" style="width: 22rem">
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-sm-3">
-                                        <span class="icon-{{ $item['icon'] }}" style="font-size: 100px"></span>
+                                        <span class="icon-{{ $item['icon'] }}" style="font-size: 100px;"></span>
                                     </div>
-                                    <div class="col-sm-9" style="padding-top: 25px">
+                                    <div class="col-sm-9" style="padding-top: 25px;">
                                         <h4>{{ __($item['name']) }}</h4>
                                     </div>
                                 </div>
